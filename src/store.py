75d559551ca8c2226e8faad84f935563891e845a
Kevin Gonzalez <kevin-951206@hotmail.com>
2025-06-13 00:07:00 -0500

    files structure updated

--- src/store.py
+++ src/store.py
@@ -4,3 +4,11 @@ class Store:
         self.town = town
         self.phone_number = phone_number
         self.store_id = store_id
+
+    def get_metadata(self):
+        return {
+            'business_name': self.business_name,
+            'town': self.town,
+            'phone_number': self.phone_number,
+            'store_id': self.store_id
+        }
